b528ce35b348750103e5b625712f1dfd39bdbc72
zhanghuifeng <zhanghuifeng@espressif.com>
2021-02-10 14:58:33 +0800

    feat:support check idf version

---
+++ tools/check_idf_version.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+EXPECT_IDF_VERSION=$1
+ESP_IDF_PATH=$2
+
+CUR_IDF_VERSION=`git -C ${IDF_PATH} show -s --pretty=format:'%H'`
+echo "Currect esp-idf version is ${CUR_IDF_VERSION}"
+
+if [[ ${EXPECT_IDF_VERSION} != ${CUR_IDF_VERSION} ]]; then
+    echo "** Warning **"
+    echo "The version of esp-idf is not expected, please set it to ${EXPECT_IDF_VERSION}, otherwise it may not run correctly"
+fi
+
